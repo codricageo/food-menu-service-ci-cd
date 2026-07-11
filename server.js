8d4618a364de8443f98ecbdd58401723c3368082
Codryka <79510335+codricageo@users.noreply.github.com>
2026-07-11 19:55:28 +0300

    Update Margherita Pizza name in menu data

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const APP_VERSION = process.env.APP_VERSION || "v2.0";
 
 // Sample menu data (no database)
 const menu = [
-  { id: 1, name: "Margherita Pizza", price: 299 },
+  { id: 1, name: "Margherita Pizzaaaaa", price: 299 },
   { id: 2, name: "Veg Burger", price: 149 },
   { id: 3, name: "Pasta Alfredo", price: 249 },
   { id: 4, name: "French Fries", price: 99 }
